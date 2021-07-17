ae0650ae0d0329223505e88ce6519ade12cd6480
= <=>
2021-07-17 20:41:41 +0200

    Add source code

---
+++ TimedLoop.ts
@@ -0,0 +1,76 @@
+type callbackFunction = (...args: any[]) => any;
+
+class TimedLoop {
+  private delay: number;
+  private numberOfIterations: number;
+  private currentIteration: number;
+  private isRunning: boolean;
+  private callback: callbackFunction;
+
+  constructor(
+    delay: number,
+    numberOfIterations: number,
+    callback: callbackFunction
+  ) {
+    this.delay = delay;
+    this.numberOfIterations = numberOfIterations;
+    this.callback = callback;
+    this.currentIteration = 0;
+  }
+
+  private timer(delay) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
+
+  private async theLoop() {
+    const numberOfIterations = this.numberOfIterations;
+    for (let i = this.currentIteration; i < numberOfIterations; i++) {
+      if (this.isRunning) {
+        this.callback(i);
+        this.currentIteration++;
+        await this.timer(this.delay);
+      } else {
+        this.stop();
+        break;
+      }
+    }
+  }
+
+  /**
+   * Starts the loop from 0.
+   */
+  public start() {
+    if (!this.isRunning) {
+      this.currentIteration = 0;
+      this.isRunning = true;
+      this.theLoop();
+    } else {
+      console.warn('You can only have one loop running in the object.');
+    }
+  }
+
+  /**
+   * Stops the loop and reintializes internal counter to 0.
+   */
+  public stop() {
+    this.currentIteration = 0;
+    this.isRunning = false;
+  }
+
+  /**
+   * Pauses the loop while keeping the internal counter intact.
+   */
+  public pause() {
+    this.isRunning = false;
+  }
+
+  /**
+   * Resumes the loop based on the internal counter.
+   */
+  public continue() {
+    this.isRunning = true;
+    this.theLoop();
+  }
+}
+
+export default TimedLoop;
